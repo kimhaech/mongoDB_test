a093c3667bde9041c507a0051d797423bbd099bb
kimhaech <epiclog@hufs.ac.kr>
2022-05-09 00:50:45 +0900

    데이터 조회

--- index.js
+++ index.js
@@ -22,20 +22,27 @@ const coinSchema = new mongoose.Schema({
 
 const Coin = mongoose.model('ctestdata', coinSchema)
 
-const bitcoin1 = new Coin({
-  date: 2022 - 12 - 01,
-  open: '2000202',
-  high: '11111111',
-  low: '13452',
-  close: '1000000',
-  vol: '3333303030',
-  marcket_cap: '11313131',
+// const bitcoin1 = new Coin({
+//   open: '2000202',
+//   high: '11115151',
+//   low: '1345244',
+//   close: '10002300',
+//   vol: '3333301130',
+//   marcket_cap: '12313131',
+// })
+// bitcoin1
+//   .save()
+//   .then(() => {
+//     console.log('성공')
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+
+Coin.find({ open: '2000202' }, 'low close', function (err, docs) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('return : ', docs)
+  }
 })
-bitcoin1
-  .save()
-  .then(() => {
-    console.log('성공')
-  })
-  .catch((err) => {
-    console.error(err)
-  })
