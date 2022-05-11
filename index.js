8ef150c59e2222bb70724d40a73cda4c28309507
kimhaech <epiclog@hufs.ac.kr>
2022-05-12 03:28:52 +0900

    입력기간의 데이터 조회

--- index.js
+++ index.js
@@ -20,8 +20,31 @@ const coinSchema = new mongoose.Schema({
   marcket_cap: { type: String },
 })
 
-const Coin = mongoose.model('costs2', coinSchema)
+// 각 월별 일자
+const calendar = {
+  1: 31,
+  2: 28,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31,
+}
+// Coin의 모델 costs
+const Coin = mongoose.model('costs', coinSchema)
 
+// if (day > 9) {
+//   // 10 이상 day
+//   let temp = year.concat('-', month).concat('-', day.toString())
+// } else {
+//   // 10 미만 day
+//   let temp = year.concat('-', month).concat('-0', day.toString())
+// }
 // const bitcoin1 = new Coin({
 //   open: '2000202',
 //   high: '11115151',
@@ -39,14 +62,90 @@ const Coin = mongoose.model('costs2', coinSchema)
 //     console.error(err)
 //   })
 
-const fields = {_id:0, greed_fear_score:0, date:0, Open:0, Low:0, Close:0, Volume:0}
+const fields = {
+  _id: 0,
+  greed_fear_score: 0,
+  Open: 0,
+  Low: 0,
+  Close: 0,
+  Volume: 0,
+}
 
-Coin.find({High: {$gte: 50000}},fields,function (err, docs) {
+const start_year = 2021
+const start_month = 3
+const start_day = 1
+const end_year = 2021
+const end_month = 5
+const end_day = 31
+
+Coin.find({}, fields, function (err, docs) {
   if (err) {
     console.log(err)
   } else {
-    for(i=0; i<docs.length; i++){
-      console.log(docs[i].toObject().High)
+    // docs -> find 결과값
+    const start_year = 2021
+    const start_month = 3
+    const start_day = 1
+    const end_year = 2021
+    const end_month = 5
+    const end_day = 31
+
+    for (i = 0; i < docs.length; i++) {
+      for (
+        s_year = start_year, s_month = start_month, s_day = start_day;
+        s_year <= end_year && s_month <= end_month && s_day <= end_day;
+        s_day++
+      ) {
+        let td = ''
+        if (s_month < 10) {
+          td = s_year.toString().concat('-0', s_month.toString())
+        } else {
+          td = s_year.toString().concat('-', s_month.toString())
+        }
+
+        if (s_day < 10) {
+          td = td.concat('-0', s_day.toString())
+        } else {
+          td = td.concat('-', s_day.toString())
+        }
+        if (s_day === calendar[s_month]) {
+          s_day = 1
+          s_month++
+        }
+
+        if (s_month === 13) {
+          s_month = 1
+          s_year++
+        }
+
+        if (docs[i].toObject().date === td) {
+          console.log(docs[i].toObject().date + ' : ' + docs[i].toObject().High)
+        }
+      }
     }
   }
 })
+
+// function get_period_data(
+//   start_year,
+//   start_month,
+//   start_day,
+//   end_year,
+//   end_month,
+//   end_day
+// ) {
+//   for (
+//     s_year = start_year, s_month = start_month, s_day = start_day;
+//     s_year === end_year && s_month === end_month && s_day === end_day;
+//     s_day++
+//   ) {
+//     if (s_day === calendar[s_month]) {
+//       s_day = 1
+//       s_month++
+//     }
+//     if (s_month === 12) {
+//       s_month = 1
+//       s_year++
+//     }
+//   }
+// }
--- mdbc/read_csv.py
+++ mdbc/read_csv.py
@@ -1,8 +1,10 @@
+from tokenize import String
 from turtle import pd
 import pandas as pd
 
 df = pd.read_csv('C:/Users/epicl/Documents/GitHub/mongoDB_test/config/fake.CSV', encoding='utf-8',dtype ={
   'greed_fear_score' : float,
+  'date' : str,
   'Open' : float,
   'High' : float,
   'Low' : float,
