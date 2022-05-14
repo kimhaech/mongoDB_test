68fc5967227c9e8766e82ed4cc5c2dbb8a88cee0
kimhaech <epiclog@hufs.ac.kr>
2022-05-15 04:28:17 +0900

    함수 생성

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ mongoose
 // 스키마 정의
 const coinSchema = new mongoose.Schema({
   date: { type: String },
-  open: { type: String },
-  high: { type: String },
-  low: { type: String },
-  close: { type: String },
-  vol: { type: String },
-  marcket_cap: { type: String },
 })
 
 // 각 월별 일자
@@ -38,116 +32,186 @@ const calendar = {
 // Coin의 모델 costs
 const Coin = mongoose.model('costs', coinSchema)
 
-const fields = {
-  _id: 0,
-  date : 1,
-  greed_fear_score: 1,
-  Open: 0,
-  High: 1,
-  Low: 0,
-  Close: 0,
-  Volume: 0,
-}
+// const fields = {
+//   _id: 0,
+//   Open: 0,
+//   Low: 0,
+//   Close: 0,
+//   Volume: 0,
+// }
 
-const start_year = 2021
-const start_month = 3
-const start_day = 1
-const end_year = 2021
-const end_month = 5
-const end_day = 31
-
-Coin.find({}, fields, function (err, docs) {
+Coin.find({}, function (err, docs) {
   if (err) {
     console.log(err)
   } else {
     // docs -> find 결과값
-    const start_year = 2021
-    const start_month = 2
-    const start_day = 5
-    const end_year = 2021
-    const end_month = 2
-    const end_day = 5
+    // for (i = 0; i < docs.length; i++) {
+    //   console.log(docs[i].toObject().date + ' : ' + docs[i].toObject().High)
+    // }
+    today = get_today()
+    console.log('today : ' + today)
 
-    for (i = 0; i < docs.length; i++) {
-      for (
-        s_year = start_year, s_month = start_month, s_day = start_day;
-        s_year <= end_year && s_month <= end_month && s_day <= end_day;
-        s_day++
-      ) {
-        let td = ''
-        if (s_month < 10) {
-          td = s_year.toString().concat('-0', s_month.toString())
-        } else {
-          td = s_year.toString().concat('-', s_month.toString())
-        }
+    const start_year = 2022
+    const start_month = 3
+    const start_day = 2
+    const end_year = 2022
+    const end_month = 3
+    const end_day = 2
 
-        if (s_day < 10) {
-          td = td.concat('-0', s_day.toString())
-        } else {
-          td = td.concat('-', s_day.toString())
-        }
-        if (s_day === calendar[s_month]) {
-          s_day = 1
-          s_month++
-        }
+    // 각 1일전, 일주일 전, 10일 전
+    const a_day_ago = get_beforedate(today[0], today[1], today[2], 1)
+    const a_week_ago = get_beforedate(today[0], today[1], today[2], 7)
+    const ten_days_ago = get_beforedate(today[0], today[1], today[2], 10)
 
-        if (s_month === 13) {
-          s_month = 1
-          s_year++
-        }
+    console.log(a_day_ago)
+    console.log(a_week_ago)
+    console.log(ten_days_ago)
 
-        if (docs[i].toObject().date === td) {
-          console.log(docs[i].toObject().date + ' : ' + docs[i].toObject().High)
-        }
-      }
-    }
+    get_period_data(
+      start_year,
+      start_month,
+      start_day,
+      end_year,
+      end_month,
+      end_day,
+      docs
+    )
   }
 })
 
+// 시작 일자로 부터 h일 전의 날짜값 반환
+function get_beforedate(y, m, d, h) {
+  let b = d - h // 기준 일 - h일 전
+  let result_date = []
+
+  if (b < 1) {
+    // b가 1보다 작은 경우 -> 전월
+    if (m == 1) {
+      m = 12
+      y -= 1
+      d = calendar[m] + b
+    } else {
+      m -= 1
+      d = calendar[m] + b
+    }
+    result_date.push(y, m, d)
+  } else {
+    d -= h
+    result_date.push(y, m, d)
+  }
+
+  return result_date
+}
+
+// start에서 end 간의 데이터 조회
+function get_period_data(
+  start_year,
+  start_month,
+  start_day,
+  end_year,
+  end_month,
+  end_day,
+  docs
+) {
+  yb = false
+  mb = false
+  end = false
+  for (
+    s_year = start_year, s_month = start_month, s_day = start_day;
+    end != true;
+    s_day++
+  ) {
+    if (s_year == end_year) {
+      yb = true
+      if (s_month == end_month) {
+        mb = true
+        if (s_day == end_day) {
+          end = true
+        }
+      }
+    }
+    let td = ''
+    if (s_month < 10) {
+      td = s_year.toString().concat('-0', s_month.toString())
+    } else {
+      td = s_year.toString().concat('-', s_month.toString())
+    }
+
+    if (s_day < 10) {
+      td = td.concat('-0', s_day.toString())
+    } else {
+      td = td.concat('-', s_day.toString())
+    }
+    if (s_day === calendar[s_month]) {
+      s_day = 1
+      s_month++
+    }
+
+    if (s_month === 13) {
+      s_month = 1
+      s_year++
+    }
+
+    // docs 하나씩 조회
+    for (i = 0; i < docs.length; i++) {
+      if (docs[i].toObject().date === td) {
+        console.log(docs[i].toObject().date + ' : ' + docs[i].toObject().High)
+      }
+    }
+  }
+}
 // function get_period_data(
 //   start_year,
 //   start_month,
 //   start_day,
 //   end_year,
 //   end_month,
-//   end_day
+//   end_day,
+//   docs
 // ) {
-//   for (
-//     s_year = start_year, s_month = start_month, s_day = start_day;
-//     s_year === end_year && s_month === end_month && s_day === end_day;
-//     s_day++
-//   ) {
-//     if (s_day === calendar[s_month]) {
-//       s_day = 1
-//       s_month++
-//     }
-//     if (s_month === 12) {
-//       s_month = 1
-//       s_year++
+//   for (i = 0; i < docs.length; i++) {
+//     for (
+//       s_year = start_year, s_month = start_month, s_day = start_day;
+//       s_year <= end_year && s_month <= end_month && s_day <= end_day;
+//       s_day++
+//     ) {
+//       let td = ''
+//       if (s_month < 10) {
+//         td = s_year.toString().concat('-0', s_month.toString())
+//       } else {
+//         td = s_year.toString().concat('-', s_month.toString())
+//       }
+
+//       if (s_day < 10) {
+//         td = td.concat('-0', s_day.toString())
+//       } else {
+//         td = td.concat('-', s_day.toString())
+//       }
+//       if (s_day === calendar[s_month]) {
+//         s_day = 1
+//         s_month++
+//       }
+
+//       if (s_month === 13) {
+//         s_month = 1
+//         s_year++
+//       }
+
+//       if (docs[i].toObject().date === td) {
+//         console.log(docs[i].toObject().date + ' : ' + docs[i].toObject().High)
+//       }
 //     }
 //   }
 // }
 
-// if (day > 9) {
-//   // 10 이상 day
-//   let temp = year.concat('-', month).concat('-', day.toString())
-// } else {
-//   // 10 미만 day
-//   let temp = year.concat('-', month).concat('-0', day.toString())
-// }
-// const bitcoin1 = new Coin({
-//   open: '2000202',
-//   high: '11115151',
-//   low: '1345244',
-//   close: '10002300',
-//   vol: '3333301130',
-//   marcket_cap: '12313131',
-// })
-// bitcoin1
-//   .save()
-//   .then(() => {
-//     console.log('성공')
-//   })
-//   .catch((err) => {
-//     console.error(err)
-//   })
+// 오늘 날짜를 반환
+function get_today() {
+  let today = new Date()
+  let dd = today.getDate()
+  let mm = today.getMonth() + 1
+  let yyyy = today.getFullYear()
+  today = yyyy + '-' + mm + '-' + dd
+
+  console.log(today)
+  return [yyyy, mm, dd]
+}
